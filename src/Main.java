5cb4109dfd3b5f3f6d2b811ce1dea2d6347074ea
access-log-parser <Olegbbbb@inbox.ru>
2023-05-15 17:16:29 +0200

    Новая ветка. Работа с числами.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
+        System.out.println("Введите два числа через Enter:");
         int number = new Scanner(System.in).nextInt();
         int number2 = new Scanner(System.in).nextInt();
 
